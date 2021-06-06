2a095171a2236e489e3113f13047baf83afeb0e5
Daniel Bykov <daniel-bykov@yandex.ru>
2021-06-07 01:21:10 +0400

    05.06.2021

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+
+
 let categoryCrane = document.querySelectorAll('.list-item');
 let descriptionCrane = document.querySelectorAll('.description-item');
 
@@ -23,8 +25,109 @@ nextRevButt.addEventListener('click', ()=>{
   prevRevButt.addEventListener('click', ()=>{
     swiper.slidePrev();
     })
- });
 
+
+
+  //---------------------------Order Input Validation
+
+  const form = document.querySelectorAll('.personal-data__input')
+  const userName = document.getElementById('user-name')
+  const phoneNumber = document.getElementById('phone-number')
+  const street = document.getElementById('street')
+  const home = document.getElementById('home')
+  const apartment = document.getElementById('apartment')
+
+
+  form.forEach((item, index) => {
+    console.log(index)
+    item.addEventListener('change', (e) => {
+      e.preventDefault();
+      console.log(item)
+      item.сlassName = "personal-data__input";
+      checkInputs(item);
+    })
+  })
+
+
+  function checkInputs(item) {
+
+    const inputOne = item.value.trim();
+
+    if (item.id != "phone-number") {
+      if (inputOne === "") {
+        setErrorFor(item)
+      }
+      else {
+        setSuccsessFor(item)
+      }
+
+    }
+    else if (!validatePhone(inputOne)) {
+      setErrorFor(item)
+    }
+    else {
+      setSuccsessFor(item)
+    }
+
+  }
+
+  function validatePhone(phone) {
+    let regex = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
+    return regex.test(phone);
+  }
+
+  function setErrorFor(input) {
+    if (input.classList.contains("personal-data__input-succsess")) {
+      input.classList.remove("personal-data__input-succsess")
+    }
+    input.classList.add('personal-data__input-error')
+  }
+
+  function setSuccsessFor(input) {
+    if (input.classList.contains("personal-data__input-error")) {
+      input.classList.remove("personal-data__input-error")
+    }
+    input.classList.add('personal-data__input-succsess')
+  }
+
+  let nav = document.querySelector('.fp-right')
+  let pageUpbutt = document.createElement('button');
+  pageUpbutt.className = "page-up";
+  pageUpbutt.innerHTML = "НАВЕРХ";
+
+  let paginationPage = document.createElement('div');
+  paginationPage.className = "pagination-page";
+  paginationPage.innerHTML = "<span class=\"pagination-page--active\">1</span><span class=pagination-page--all>/9</span> ";
+  nav.append(paginationPage)
+
+  nav.append(pageUpbutt)
+
+  let activeSlideNumber = 0;
+
+  let pageUp =  document.querySelector('.page-up');
+    pageUp.addEventListener('click', function(e) {
+    fullpage_api.moveTo(1); });
+
+    let section = document.querySelectorAll('.section');
+    let activeSlide = document.querySelector('.pagination-page--active');
+
+    document.body.addEventListener('wheel', function(e) {
+      section.forEach((item, index) => {
+        if(item.classList.contains("active")){
+           activeSlide.innerHTML = fullpage_api.getActiveSection().index+1;
+        }
+      })
+    })
+
+    document.querySelector(".fp-right").addEventListener('click', function(e) {
+      section.forEach((item, index) => {
+        if(item.classList.contains("active")){
+           activeSlide.innerHTML = fullpage_api.getActiveSection().index+1;
+        }
+      })
+    })
+      
+ });
 
 
 
@@ -39,4 +142,14 @@ nextRevButt.addEventListener('click', ()=>{
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+
+
+new fullpage('#fullpage', {
+	//options here
+  
+  navigation: true,
+  navigationPosition: 'right',
+  // scrollOverflow:true,
+  bigSectionsDestination: 'top',
 });
